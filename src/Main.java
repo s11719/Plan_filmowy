5d83b020300bcee04fc27aa4d29afc8fa4c2e9fb
peteaq <peteaq@PeTeAQ-Linux-Laptop>
2015-01-15 22:23:40 +0100

    Implementacja połączenia z bazą danych.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+import repositories.C_Repozytorium_Ogólne;
+import repositories.impl.Repozytorium_Ogólne;
+import unitofwork.IUnitOfWork;
+import unitofwork.UnitOfWork;
 import domain.*;
 
 
@@ -7,6 +16,7 @@ public class Main {
 		
 		System.out.println("To będzie repozytorium planu filmowego.");
 	
+/*
 	Osoba Piotrek = new Osoba();
 	Pracownik Reżyser = new Pracownik();
 	Lokacja Gdynia = new Lokacja();
@@ -20,11 +30,35 @@ public class Main {
 	Piotrek.setNazwisko("Ptaszyński");
 	Piotrek.setData_urodzenia("1986-07-31");
 	Piotrek.setPracownik(Reżyser);
+*/
+		
+	String url="jdbc:hsqldb:mem:test"+","+"SA"+"";
+	Pracownik pptaszynski = new Pracownik();
+	pptaszynski.setFunkcja("Reżyser");
 	
-	
+	try {
+		
+		Connection connection = DriverManager.getConnection(url);
+		IUnitOfWork uow = new UnitOfWork(connection);
+		
+		C_Repozytorium_Ogólne catalog = new Repozytorium_Ogólne(connection, uow);
+		
+		
+		catalog.getPracownicy().save(pptaszynski);
+		
+		List<Pracownik> usersFromDb= catalog.getPracownicy().getAll();
+		
+		for(Pracownik userFromDb: usersFromDb)
+			System.out.println(userFromDb.getId()+" "+userFromDb.getFunkcja());
+						
+		
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	System.out.println("To tyle :)");
+}
 	
 	
 	
 	}
 	
-}
